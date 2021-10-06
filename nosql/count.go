51b8e4d6ff7ed4dc0bc763fd1a38f804bb0f6dd6
Justin Robertson <justinr@uprate.co.za>
2021-10-06 13:22:55 +0200

    added count function for nosql interface

---
+++ nosql/count.go
@@ -0,0 +1,35 @@
+package nosql
+
+import (
+	"fmt"
+	"github.com/go-diary/diary"
+	"github.com/go-uniform/uniform"
+	"go.mongodb.org/mongo-driver/bson"
+	"time"
+)
+
+type CountRequest struct {
+	Database string
+	Collection string
+	Query bson.D
+}
+
+func (m *nosql) Count(timeout time.Duration, database, collection string, query bson.D) (count int64) {
+	subj := "action.nosql.count"
+	if m.serviceId != "" {
+		subj = fmt.Sprintf("%s.%s", m.serviceId, subj)
+	}
+
+	if err := m.c.Request(m.p, subj, timeout, uniform.Request{
+		Model: CountRequest{
+			Database: database,
+			Collection: collection,
+			Query: query,
+		},
+	}, func(r uniform.IRequest, p diary.IPage) {
+		r.Read(&count)
+	}); err != nil {
+		panic(err)
+	}
+	return
+}

90f1e4e43b80b724684e4f55263518e7039f6551
Justin Robertson <justinr@uprate.co.za>
2023-03-30 19:31:07 +0200

    created a very basic docker-compose script

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 * [go-nats](https://github.com/nats-io/nats.go)
 * [go-diary](https://github.com/go-diary/diary)
+
+```
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/uniform-nats.key -out /etc/ssl/certs/uniform-nats.crt
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/uniform-amqps.key -out /etc/ssl/certs/uniform-amqps.crt
+sudo chmod +r /etc/ssl/private/uniform-nats.key
+sudo chmod +r /etc/ssl/private/uniform-amqps.key
+```
--- types.go
+++ types.go
@@ -1,6 +1,10 @@
 package uniform
 
-import "github.com/go-diary/diary"
+import (
+	"github.com/go-diary/diary"
+	"reflect"
+	"time"
+)
 
 // A package shorthand for a map[string]interface
 type M map[string]interface{}
@@ -13,3 +17,53 @@ type Q map[string][]string
 
 // A package shorthand for func(r IRequest, p diary.IPage)
 type S func(r IRequest, p diary.IPage)
+
+type DateTime struct {
+	UtcTime time.Time
+
+	Timezone           string
+	TimezoneAdjustment time.Duration
+	TimezoneTime       time.Time
+
+	DaylightSavings           bool
+	DaylightSavingsStart      time.Time
+	DaylightSavingsEnd        time.Time
+	DaylightSavingsAdjustment time.Duration
+	LocalTime                 time.Time
+}
+
+type Money struct {
+	CurrencyCode   string
+	CurrencySymbol string
+	DateTime       DateTime
+
+	WholeNumber int64
+	Precision   byte
+	Value       float64
+
+	Display string
+}
+
+type Real struct {
+	WholeNumber int64
+	Precision   byte
+	Value       float64
+}
+
+type Protected struct {
+	Kind      reflect.Kind
+	Hash      string
+	Encrypted []byte
+}
+
+func (p *Protected) CompareHash(value string) bool {
+	panic("not yet implemented")
+}
+
+func (p *Protected) Decrypt() interface{} {
+	panic("not yet implemented")
+}
+
+func NewProtectedValue(value interface{}) Protected {
+	panic("not yet implemented")
+}
